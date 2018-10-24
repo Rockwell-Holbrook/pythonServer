58c3b160caaa0c86436de86c2a1d0c0f985355e0
Rockwell Holbrook <Rockwell.holbrook@gmail.com>
2018-10-24 11:24:00 -0600

    Added a few basic commands.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-
+import sys
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
+messages = []
 
 @app.route('/')
 def sessions():
@@ -15,7 +16,28 @@ def messageReceived(methods=['GET', 'POST']):
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
+    
+    try:
+        print(str(json['message']))
+
+        if (str(json['message']) == "!exit"):
+            quit()
+
+        if (str(json['message']) == "!username"):
+            json['message'] = "Your Username is: " + json['user_name']
+        
+        elif (str(json['message']) == "!help"):
+            json['message'] = "Commands: !username, !clear, and of course !help!"
+
+        else:
+            myMessage = str(json['user_name']) + " " + str(json['message'])
+
+        messages.append(myMessage)
+        print(messages)
+        socketio.emit('my response', json, callback=messageReceived)
+
+    except:
+        socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
